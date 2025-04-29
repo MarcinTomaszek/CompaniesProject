f506e3265f2675396a286d18f580eb9fed0935b5
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2025-04-29 16:29:13 +0200

    added unit tests

--- Infrastructure/EF/AppDbContext.cs
+++ Infrastructure/EF/AppDbContext.cs
@@ -6,12 +6,31 @@ namespace Infrastructure.EF;
 
 public class AppDbContext : IdentityDbContext<UserEntity>
 {
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected AppDbContext()
+    {
+    }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         var adminId = "eac0adc0-c5fc-4765-9f9d-b1ff1f8794c8";
         var adminCreatedAt = new DateTime(2025, 4, 8);
         var hash = "AQAAAAIAAYagAAAAEGUrhuLXVMtD5o6jaa8JDUduAgPQFlY6biIUDgo7QQNYCSkoBju+mphU+qp8CnsB9Q==";
+        string password = "1234!";
+        
+        // Tworzymy instancję PasswordHasher
+        var hasher = new PasswordHasher<object>();
+        
+        // Generujemy hash
+        string passwordHash = hasher.HashPassword(null, password);
+        
+        // Wyświetlamy wynik
+        Console.WriteLine("Hash dla hasła: " + passwordHash);
         
         var adminUser = new UserEntity()
         {
---
+++ Tests/AppUsersControllerTests.cs
@@ -0,0 +1,70 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json.Nodes;
+using Infrastructure.EF;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi;
+using WebApi.Dto;
+
+namespace Test;
+
+public class AppUsersControllerTests: IClassFixture<AppTestFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private readonly AppTestFactory<Program> _app;
+    private readonly AppDbContext _context;
+
+    public AppUsersControllerTests(AppTestFactory<Program> app)
+    {
+        _app = app;
+        _client = app.CreateClient();
+        var hasher = new PasswordHasher<UserEntity>();
+        
+        using (var scope = app.Services.CreateScope())
+        {
+            _context = scope.ServiceProvider.GetService<AppDbContext>();
+            _context.Users.Add(new UserEntity()
+            {
+                Id = "eac0adc0-c5fc-4765-9f9d-b1ff1f8794c8",
+                Email = "admin@wsei.edu.pl",
+                NormalizedEmail = "ADMIN@WSEI.EDU.PL",
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                ConcurrencyStamp = "eac0adc0-c5fc-4765-9f9d-b1ff1f8794c8",
+                SecurityStamp = "eac0adc0-c5fc-4765-9f9d-b1ff1f8794c8",
+                EmailConfirmed = true,
+                PasswordHash = "AQAAAAIAAYagAAAAEOrArrSG1swr5b94IyFxxXI9wv/pMOWdiSK3LvAtL3VoMmk6sTFHTvhuRqAesmP/Ag=="
+            });
+            _context.SaveChanges();
+        }
+    }
+
+    [Fact]
+    public async void TestValidLogin()
+    {
+        var loginBody = new LoginDto
+        {
+            Login = "admin",
+            Password = "1234!"
+        };
+        var result = await _client.PostAsJsonAsync("/api/users/login", loginBody);
+        
+        Assert.NotNull(result);
+        Assert.Equal(result.StatusCode, HttpStatusCode.OK);
+        JsonNode node = JsonNode.Parse(await result.Content.ReadAsStringAsync());
+        var token = node["token"].AsValue().ToString();
+        Assert.NotNull(token);
+    }
+    
+    [Fact]
+    public async void TestBookController()
+    {
+        var result = await _client.GetAsync("/api/books");
+        
+        Assert.NotNull(result);
+        Assert.Equal(result.StatusCode, HttpStatusCode.Unauthorized);
+     
+            
+    }
+}
