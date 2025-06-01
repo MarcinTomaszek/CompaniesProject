4f6cb0bce1616a8a00b5f13637de9c510e244efa
Marcin Tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2025-06-01 21:26:58 +0200

    added Endpoint app/Companies/.

--- WebApi/Controllers/CompaniesController.cs
+++ WebApi/Controllers/CompaniesController.cs
@@ -1,31 +1,68 @@
+using System.ComponentModel;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Infrastructure.EF;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
     [ApiController]
     [Authorize(Policy = "Bearer")]
-    public class CompaniesController(UserManager<UserEntity> userManager) : ControllerBase
+    public class CompaniesController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetCompanies()
+        
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<UserEntity> _userManager;
+        public CompaniesController(AppDbContext dbContext, UserManager<UserEntity> userManager)
         {
-            Console.WriteLine(GetCurrentUser().UserName);
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+        
+        
+        
+        [HttpGet]
+        [AllowAnonymous]
+        [EndpointDescription("Get list of companies paginated 20 companies per page")]
+        public IActionResult GetCompanies(
+            [FromQuery, Description("Page number of the companies list (default is 1).")] int page = 1,
+            [FromQuery, Description("Number of companies per page (default is 20).")] int pageSize = 20)
+
+        {
             
+            var companies = _dbContext.Companies
+                .OrderBy(c => c.Rank) 
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(c => new
+                {
+                    c.Rank,
+                    c.Name,
+                    c.Url,
+                    c.State,
+                    c.City
+                })
+                .ToList();
+            
+            var totalCompanies = _dbContext.Companies.Count();
+
             return Ok(new
             {
-                Title = "C#",
-                Autor = "Bloch" 
+                Page = page,
+                PageSize = pageSize,
+                TotalCount = totalCompanies,
+                Companies = companies
             });
         }
          
-        private UserEntity? GetCurrentUser()
+        [HttpGet]
+        [Microsoft.AspNetCore.Mvc.Route("User")]
+        public UserEntity? GetCurrentUser()
         {
             var user = HttpContext.User.Identity as ClaimsIdentity;
 
@@ -33,7 +70,7 @@ namespace WebApi.Controllers
             {
                 string username = user.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Name)?.Value;
 
-                return userManager.FindByNameAsync(username).Result;
+                return _userManager.FindByNameAsync(username).Result;
             }
 
             return null;
